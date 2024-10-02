159f2ee816b903bedd229a2377ad92db49730cfc
TairoSilva <162700891+Icaro16@users.noreply.github.com>
2024-10-01 23:44:42 -0300

    Add files via upload

---
+++ Atividade Modal/modal.js
@@ -0,0 +1,17 @@
+function abrirModal(){
+
+    let modal = document.getElementById('janela-modal')
+
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+
+            modal.classList.remove('abrir')
+
+        }
+
+    })
+    
+}
